2170c0cfff6e362c1d9e27fba0f9e3ea0ff81b72
Yap Ming Qian <yapmingqian@gmail.com>
2021-07-08 02:55:17 +0800

    empty build

---
+++ ChargEVCompanionApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ChargEVCompanionApp.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
