b4648e17b397c9a26525245cc271611d63f6f6c8
Yap Ming Qian <yapmingqian@gmail.com>
2021-07-08 02:06:53 +0800

    Add project files.

---
+++ ChargEVCompanionApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChargEVCompanionApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChargEVCompanionApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
