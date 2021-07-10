c2120cd774bc9630e2488f55e76b7a194a86e729
Yap Ming Qian <yapmingqian@gmail.com>
2021-07-11 04:06:22 +0800

    searchbar

--- ChargEVCompanionApp/ViewModels/AddStationViewModel.cs
+++ ChargEVCompanionApp/ViewModels/AddStationViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using ChargEVCompanionApp.Models;
+using Plugin.Geolocator;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ChargEVCompanionApp.ViewModels
 {
@@ -12,5 +15,35 @@ namespace ChargEVCompanionApp.ViewModels
 
         public Venue Venue { get => venue; set => SetProperty(ref venue, value); }
 
+        //private Command refreshCommand;
+
+        //public ICommand RefreshCommand
+        //{
+        //    get
+        //    {
+        //        if (refreshCommand == null)
+        //        {
+        //            refreshCommand = new Command(Refresh);
+        //        }
+
+        //        return refreshCommand;
+        //    }
+        //}
+
+        //private async void Refresh()
+        //{
+        //    venueListView.ItemsSource = null;
+
+        //    var updLocator = CrossGeolocator.Current;
+        //    var updPosition = await updLocator.GetPositionAsync();
+        //    var updVenues = await VenueService.GetVenues(updPosition.Latitude, updPosition.Longitude);
+
+        //    venueListView.ItemsSource = updVenues.Where(x => x.categories.Count() > 0).Where(x => x.location.address != null);
+        //    venueListView.IsRefreshing = false;
+        //}
+
+        //private bool isRefreshing;
+
+        //public bool IsRefreshing { get => isRefreshing; set => SetProperty(ref isRefreshing, value); }
     }
 }
