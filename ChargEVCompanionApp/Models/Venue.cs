19068bba8efbe25e7cbb8486a689bc6fe54e476c
Yap Ming Qian <yapmingqian@gmail.com>
2021-07-10 17:44:41 +0800

    latest

---
+++ ChargEVCompanionApp/Models/Venue.cs
@@ -0,0 +1,15 @@
+﻿using ChargEVCompanionApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargEVCompanionApp.Models
+{
+    public class Venue
+    {
+        public static string GenerateURl(double latitude, double longtitude)
+        {
+            return string.Format(Constants.VENUE_SEARCH, latitude, longtitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
